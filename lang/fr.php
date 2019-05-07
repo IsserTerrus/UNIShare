a07707a9a5b0a4fce0f14b286f2dc44eba9ac6e6
Gautou <44871164+Gautou@users.noreply.github.com>
2019-05-07 17:20:15 +0200

    Ajout fr.php

---
+++ lang/fr.php
@@ -0,0 +1,20 @@
+<?php
+// UNISHARE en FR
+$UNISHARE_LNG["Site_Title"] = "UNISHARE";
+$UNISHARE_LNG["Site_Footer"] = "UNISHARE 0.1";
+
+//Alertes
+$UNISHARE_LNG["ALERT_No_Title"] = "Partage quelque chose qui te tient à c&oelig;ur !";
+
+//Mastodon
+$UNISHARE_LNG["Share_To_Mastodon"] = "Partage sur Mastodon";
+$UNISHARE_LNG["Comment_With_Mastodon"] = "Commente sur Mastodon";
+
+//Diaspora
+$UNISHARE_LNG["Share_To_Diaspora"] = "Partage sur Diaspora";
+
+// GAFAM
+$UNISHARE_LNG["Share_To_FB"] = "Partage sur Facebook";
+$UNISHARE_LNG["Share_To_TW"] = "Partage sur Twitter";
+$UNISHARE_LNG["Share_To_Pin"] = "Partage sur Pinterest";
+?>
