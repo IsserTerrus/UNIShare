075d721f54f266142c3fef512ebbbd612b45e3ba
IsserTerrus <isserterrus@teddybeard.eu>
2019-05-08 13:04:19 +0200

    Update Debug + Javascript

--- js/unishare.js
+++ js/unishare.js
@@ -3,6 +3,18 @@
 function UNISHARE_Val(elemid){
     return document.getElementById(elemid).value
 };
+function UNISHAIRE_ExtractInstance(userinstancelabel){
+    var InstanceRes = "";
+    if(userinstancelabel.includes("@"))
+    {
+        InstanceRes = userinstancelabel.split("@")[1];
+    }
+    else
+    {
+        InstanceRes = userinstancelabel;
+    }
+    return "https:"+InstanceRes;
+};
 function GAFAM_Share_FB(SHR_Text, SHR_URL){
     var URLDist = "https://www.facebook.com/sharer.php";
     URLDist += "?quote="+encodeURIComponent(SHR_Text);
@@ -16,4 +28,29 @@ function GAFAM_Share_TW(SHR_Text, SHR_URL){
     URLDist += "&url="+encodeURIComponent(SHR_URL);
     window.location.href =  URLDist;
     return false;
+};
+
+function FEDI_Share_Masto(SHR_Text, SHR_URL, SHR_Instance){
+    var URLDist = UNISHAIRE_ExtractInstance(SHR_Instance)+"/share";
+    URLDist += "?text="+encodeURIComponent(SHR_Text);
+    URLDist += "&url="+encodeURIComponent(SHR_URL);
+    window.location.href =  URLDist;
+    return false;
+
+};
+
+function FEDI_Share_Diaspora(SHR_Text, SHR_URL, SHR_Instance){
+    var URLDist = UNISHAIRE_ExtractInstance(SHR_Instance)+"/bookmarklet";
+    URLDist += "?title="+encodeURIComponent(SHR_Text);
+    URLDist += "&url="+encodeURIComponent(SHR_URL);
+    window.location.href =  URLDist;
+    return false;
+};
+
+function FEDI_Comment_Masto(SHR_URL, SHR_Instance){
+    var URLDist = UNISHAIRE_ExtractInstance(SHR_Instance)+"/authorize_interaction";
+    URLDist += "?uri="+encodeURIComponent(SHR_URL);
+    window.location.href =  URLDist;
+    return false;
+
 };
