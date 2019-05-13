097ef7d0e129ec734da00f2f76055edb23c65c7f
Gautou <44871164+Gautou@users.noreply.github.com>
2019-05-13 11:39:00 +0200

    corrections

--- js/unishare.js
+++ js/unishare.js
@@ -84,7 +84,7 @@ function readCookie(nom) {
     var nomE = nom + "=";
     var cookies = document.cookie.split(';');
     for (var i=0 ; i<cookies.length ; i++) {
-        var cookie = cookies[i]
+        var cookie = cookies[i];
         while (cookie.charAt(0)==' ') {
             cookie = cookie.substring(1,cookie.length);
         }
@@ -93,11 +93,11 @@ function readCookie(nom) {
         }
     }
     return "";
-}
+};
 
 function afficherCookie() {
     var user = readCookie('yourInstance');
     if (user != "") {
       document.getElementById('InstanceUser').value = user;
     }
-  } 
+};
